26f6f7c407913f2d880fbc52a0d7673ba99efcd0
fullstack412 <jaclindev99@gmail.com>
2024-04-29 13:34:34 -0400

    wip: update deploy script

--- scripts/deployArb.py
+++ scripts/deployArb.py
@@ -4,7 +4,7 @@ def main():
     with networks.parse_network_choice("arbitrum:mainnet:alchemy") as provider:
         acct = accounts.load("deployer_account")
         print(acct.balance)
-        compass = "0x05d7b3A021DAFA1A52Aef09B8057493847cb6800"
+        compass = "0x1F6dD66D5dFAB320266864F09c4a0497ee4b7818"  # new compass address arbitrum-main
         competitionArb = project.competitionArb.deploy(compass, sender=acct)
         print(competitionArb)
 
--- scripts/deployEth.py
+++ scripts/deployEth.py
@@ -4,10 +4,10 @@ def main():
     with networks.parse_network_choice("ethereum:mainnet:alchemy") as provider:
         acct = accounts.load("deployer_account")
         print(acct.balance)
-        compass = ""
+        compass = "0x652Bf77d9F1BDA15B86894a185E8C22d9c722EB4"  # new compass eth-main
         reward = "0xdAC17F958D2ee523a2206206994597C13D831ec7"   # USDT
         decimals = 6
-        factory = "0x5ae3ea1d72cAb91ADfBc7E95e879b729D3ebDD03"
+        factory = ""    # need to wait for the curve healthy juice bot
         competitionEth = project.competitionEth.deploy(compass, reward, decimals, factory, sender=acct)
         print(competitionEth)
 
