bdcd3d83c7d540e924afb15d7e3603c9a390a866
fullstack412 <jaclindev99@gmail.com>
2024-04-30 02:36:20 -0400

    fix deploy script

--- scripts/deployEth.py
+++ scripts/deployEth.py
@@ -6,9 +6,9 @@ def main():
         print(acct.balance)
         compass = "0x652Bf77d9F1BDA15B86894a185E8C22d9c722EB4"  # new compass eth-main
         reward = "0xdAC17F958D2ee523a2206206994597C13D831ec7"   # USDT
-        admin = ""
-        decimals = 6
         factory = "0x82E2c99AD31e6119D874B08fbd2C3f9F9fbD1aD8"  # need to wait for the curve healthy juice bot
-        competitionEth = project.competitionEth.deploy(compass, reward, decimals, factory, admin, sender=acct)
+        admin = ""
+        
+        competitionEth = project.competitionEth.deploy(compass, reward, factory, admin, sender=acct)
         print(competitionEth)
 
